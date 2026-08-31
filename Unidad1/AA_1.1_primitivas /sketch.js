f6661ef7076034cf20d66a007fce3d21056135be
johanruiz-cmd <251k0030@itscarrillopuerto.edu.mx>
2026-08-30 21:57:28 -0500

    Update sketch.js

--- Unidad1/AA_1.1_primitivas /sketch.js
+++ Unidad1/AA_1.1_primitivas /sketch.js
@@ -1,18 +1,18 @@
 function setup() {
-  createCanvas(400, 500);
+  createCanvas(400, 400);
   
 // ANTENAS 
- background('blue');
-  stroke(255, 204, 0);      
+ background(30,41,59);
+  stroke(255, 204, 0); //amarillo     
   strokeWeight(4);         
-  line(180, 130, 160, 90);   
-  line(220, 130, 240, 90);
+  line(180, 130, 160, 90);  //antena izquierda 
+  line(220, 130, 240, 90);  //antena derecha 
 
   // CABEZA Y CUERPO  
  fill(71, 85, 105);
-  stroke(148, 163, 184);     
+  stroke(148, 163, 184);//borde claro     
   strokeWeight(2);
-  rect(150, 130, 100, 80, 10);
+  rect(150, 130, 100, 80, 10);//cabeza 
 
    fill(51, 65, 85);          
   rect(130, 220, 140, 120, 5);  
@@ -21,19 +21,22 @@ function setup() {
   fill(56, 189, 248); 
   stroke(255);               
   strokeWeight(2);
-  ellipse(175, 165, 24, 24); 
-  ellipse(225, 165, 24, 24); 
+  ellipse(175, 165, 24, 24); //ojo izquierdo
+  ellipse(225, 165, 24, 24); //ojo derecho
 
   // CUELLO / DETALLE (TRIÁNGULO)
   fill(239, 68, 68);     
   stroke(185, 28, 28);
   strokeWeight(1);
-  triangle(200, 220, 179, 210, 219, 210);
+  triangle(200, 220, 179, 210, 219, 210); //triangulo
 
 // DETALLE EXTRA: BOCA 
   stroke(255, 204, 0);
   strokeWeight(3);
-  line(180, 190, 220, 190);
+  line(180, 190, 220, 190);//sonrisa
 }
+
+
+
 
 
